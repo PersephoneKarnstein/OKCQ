ab003efd7f3c1fee80c1a649c9b38ef5ba575d4e
PersephoneKarnstein <persephone.carmilla.karnstein@gmail.com>
2019-10-27 19:11:59 -0700

    added a gitignore; also reformatted the code to hopefully catch session expiration and questions with no question text

--- Untitled-1.py
+++ Untitled-1.py
@@ -20,59 +20,100 @@ firefox_profile.set_preference('dom.ipc.plugins.enabled.libflashplayer.so', 'fal
 driver = webdriver.Firefox(firefox_profile=firefox_profile)
 
 actions = ActionChains(driver)
-wait = WebDriverWait(driver, 40)
+wait = WebDriverWait(driver, 20)
 
 url = "https://www.okcupid.com/questions?rqid="
 num = 1
 
 
 class get_Login:
-	global usrname, pwd
-	def __init__(self):
-		self.root = Tk()
-		self.label = Label(self.root, text="Enter your OKCupid login")
-		self.label.pack(side=TOP)
-		self.usrlabel = Label(self.root, text="Email:")
-		self.usrlabel.pack()
-		self.f = Entry(self.root)
-		self.f.pack()
-		self.pwdlabel = Label(self.root, text="Password:")
-		self.pwdlabel.pack()
-		self.e=Entry(self.root, show="*")
-		self.e.pack()
-		self.button = Button(self.root, text="Go", command=self.get_creds)
-		self.button.pack()
-		self.root.attributes("-topmost", True)
-		self.root.mainloop()
+    global usrname, pwd
+    def __init__(self):
+        self.root = Tk()
+        self.label = Label(self.root, text="Enter your OKCupid login")
+        self.label.pack(side=TOP)
+        self.usrlabel = Label(self.root, text="Email:")
+        self.usrlabel.pack()
+        self.f = Entry(self.root)
+        self.f.pack()
+        self.pwdlabel = Label(self.root, text="Password:")
+        self.pwdlabel.pack()
+        self.e=Entry(self.root, show="*")
+        self.e.pack()
+        self.button = Button(self.root, text="Go", command=self.get_creds)
+        self.button.pack()
+        self.root.attributes("-topmost", True)
+        self.root.mainloop()
 
-	def get_creds(self):
-		globals()["username"] = self.f.get()
-		globals()["password"] = self.e.get()
+    def get_creds(self):
+        globals()["username"] = self.f.get()
+        globals()["password"] = self.e.get()
 
-		driver.get(url+"1")
-		time.sleep(2)
-		username_field = driver.find_element_by_class_name("login2017-username")
-		password_field = driver.find_element_by_class_name("login2017-password")
+        driver.get(url+"1")
+        time.sleep(2)
+        username_field = driver.find_element_by_class_name("login2017-username")
+        password_field = driver.find_element_by_class_name("login2017-password")
 
-		username_field.send_keys(username)
-		driver.implicitly_wait(1)
+        username_field.send_keys(username)
+        driver.implicitly_wait(1)
 
-		password_field.send_keys(password)
-		driver.implicitly_wait(1)
+        password_field.send_keys(password)
+        driver.implicitly_wait(1)
 
-		driver.find_element_by_class_name("login2017-actions-button").click() #log in
+        driver.find_element_by_css_selector(".banner-close-button").click()
+        driver.implicitly_wait(2)
 
-		self.root.destroy()
+        time.sleep(2)
+        driver.find_element_by_class_name("login2017-actions-button").click() #log in
+
+        self.root.destroy()
 
 app = get_Login()
-
+# print(username, password)
 f = open("okc_questions.txt", "a")
 
-for num in range(1, 464459, 1):
+def session_cycle(usrname=username, psswrd=password):
+    driver.quit()
+    driver = webdriver.Firefox(firefox_profile=firefox_profile)
+
+    driver.get(url+"1")
+    time.sleep(2)
+    username_field = driver.find_element_by_class_name("login2017-username")
+    password_field = driver.find_element_by_class_name("login2017-password")
+
+    username_field.send_keys(username)
+    driver.implicitly_wait(1)
+
+    password_field.send_keys(password)
+    driver.implicitly_wait(1)
+
+    driver.find_element_by_css_selector(".banner-close-button").click()
+    driver.implicitly_wait(2)
+
+    time.sleep(2)
+    driver.find_element_by_class_name("login2017-actions-button").click()
+
+def get_question(num, f=f, _await_val="question"):
     driver.get(url+str(num))
-    await_question = wait.until(ec.visibility_of_element_located((By.CLASS_NAME, "questionspage-multipartquestion-questiontext")))
-    ActionChains(driver).move_to_element(await_question).perform()
+
+    if _await_val=="question":
+        await_val = wait.until(ec.visibility_of_element_located((By.CLASS_NAME, "questionspage-multipartquestion-questiontext")))
+    elif _await_val=="answers":
+        await_val = wait.until(ec.visibility_of_element_located((By.CLASS_NAME, "questionspage-multipartquestion-answers-answer-label")))
+    else: raise EXCEPTION
+
+    ActionChains(driver).move_to_element(await_val).perform()
     soup = BeautifulSoup(driver.page_source, features="lxml")
     question = soup.find("h1", "questionspage-multipartquestion-questiontext").get_text()
-    f.write(f'{num}:"{question}"\n')
+    answers = [a.get_text() for a in soup.find_all("div", "questionspage-multipartquestion-answers-answer-label")]
+    ans_str = '"'+'", "'.join(answers)+'"'
+    f.write(str(num)+':{question: "'+question+'", answers:{'+ans_str+'}}\n')
 
+for num in range(3031, 464459, 1):
+    try:
+        get_question(num)
+    except ConnectionRefusedError:
+        session_cycle()
+        get_question(num)
+    except TimeoutException:
+        get_question(num, _await_val="answers")
